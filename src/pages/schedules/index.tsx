90590efba588fcf6c4f738a2a4157530f2265258
santanarscs <raphaelstn@gmail.com>
2021-06-16 18:29:47 -0300

    update  list schedule page

--- src/pages/schedules/index.tsx
+++ src/pages/schedules/index.tsx
@@ -2,6 +2,10 @@ import { Sidebar } from "../../components/Sidebar";
 import { PlusIcon } from '@heroicons/react/outline'
 import NextLink from 'next/link'
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { GetServerSideProps } from "next";
+import { useSchedules } from "../../services/hooks/useSchedule";
+import { useState } from "react";
 
 type Schedule = {
   id: string;
@@ -17,6 +21,9 @@ type ScheduleListProps = {
 
 
 export default function ScheduleList({schedules}: ScheduleListProps) {
+  const [page, setPage] = useState(1)
+  const {data, isLoading, error, isFetching, refetch} = useSchedules(page)
+  console.log(data)
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -24,50 +31,53 @@ export default function ScheduleList({schedules}: ScheduleListProps) {
         <Sidebar />
         <div className="flex flex-1 flex-col rounded-md bg-gray-100 p-4">
           <div className="flex justify-between w-full items-center mb-8 ">
-            <h1 className="text-2xl font-normal text-gray-600">Agendamentos</h1>
-            <NextLink href="schedules/create" passHref>
+            <h1 className="text-2xl font-normal text-gray-600">
+              Agendamentos
+            </h1>
+            <NextLink href="/schedules/create" passHref>
               <a className=" flex justify-center items-center  py-2 px-3 rounded-md text-sm bg-blue-500 text-white">
                 <PlusIcon className="h-5 w-5 mr-2" />
                 <span className="uppercase">Criar novo</span>
               </a>
             </NextLink>
           </div>
-          <table className="min-w-max w-full table-auto">
-            <thead>
-              <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                <th className="py-3 px-6 text-left">#</th>
-                <th className="py-3 px-6 text-left">Nome</th>
-                <th className="py-3 px-6 text-left">Descrição</th>
-                <th className="py-3 px-6 text-left">Repetição</th>
-              </tr>
-            </thead>
-            <tbody className="text-gray-600 text-sm font-light">
-              <tr className="border-b border-gray-200 hover:bg-gray-100">
-                <td className="py-3 px-6 text-left whitespace-nowrap" >1</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Agendamento 001</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Descrição do agendamento</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Diário</td>
-              </tr>
-              <tr className="border-b border-gray-200 hover:bg-gray-100">
-                <td className="py-3 px-6 text-left whitespace-nowrap" >1</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Agendamento 001</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Descrição do agendamento</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Diário</td>
-              </tr>
-              <tr className="border-b border-gray-200 hover:bg-gray-100">
-                <td className="py-3 px-6 text-left whitespace-nowrap" >1</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Agendamento 001</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Descrição do agendamento</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Diário</td>
-              </tr>
-              <tr className="border-b border-gray-200 hover:bg-gray-100">
-                <td className="py-3 px-6 text-left whitespace-nowrap" >1</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Agendamento 001</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Descrição do agendamento</td>
-                <td className="py-3 px-6 text-left whitespace-nowrap" >Diário</td>
-              </tr>
-            </tbody>
-          </table>
+          {isLoading ? (
+              <div className="w-full flex flex-col justify-center items-center">
+                <svg className="animate-spin -ml-1 mr-3 h-14 w-14 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+              </div>
+          ) : (
+          <>
+            <table className="min-w-max w-full table-auto">
+              <thead>
+                <tr className="bg-gray-300 text-gray-600 uppercase text-sm leading-normal">
+                  <th className="py-3 px-6 text-left">#</th>
+                  <th className="py-3 px-6 text-left">Nome</th>
+                  <th className="py-3 px-6 text-left">Descrição</th>
+                  <th className="py-3 px-6 text-left">Repetição</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-600 text-sm font-light">
+                {data.schedules.map(schedule => (
+                  <tr key={schedule.id} className="border-b border-gray-200 hover:bg-gray-200">
+                    <td className="py-3 px-6 text-left whitespace-nowrap" >{schedule.id}</td>
+                    <td className="py-3 px-6 text-left whitespace-nowrap" >{schedule.title}</td>
+                    <td className="py-3 px-6 text-left whitespace-nowrap" >{schedule.description}</td>
+                    <td className="py-3 px-6 text-left whitespace-nowrap" >{schedule.repeat}</td>
+                  </tr>
+                ))}
+                
+              </tbody>
+            </table>
+            <Pagination 
+              totalCountOfRegisters={data.totalCount}
+              currentPage={page}
+              onPageChange={setPage}
+            />
+          </>
+          )}
         </div>
       </div>
     </div>
