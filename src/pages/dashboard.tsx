4c800b8059095efa5c80fbb7c339e9a498c5a1ed
santanarscs <raphaelstn@gmail.com>
2021-06-30 18:04:56 -0300

    add default layout in dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,9 @@
-import { Sidebar } from "../components/Sidebar";
+import { DefaultLayoutComponent } from "../components/DefaultLayout";
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex w-full my-6 mx-auto px-6">
-        <Sidebar />
-        <h1>Dashboard</h1>
-      </div>
-    </div>
+    <DefaultLayoutComponent>
+      <h1>Dashboard</h1>
+    </DefaultLayoutComponent>
   )
 }
