062dc08ce52955c21fa36c60936b5f54ef3a70b0
santanarscs <raphaelstn@gmail.com>
2021-07-06 17:45:26 -0300

    show jobs in detail schedule

--- src/pages/schedules/[id]/detail.tsx
+++ src/pages/schedules/[id]/detail.tsx
@@ -22,11 +22,30 @@ type Schedule = {
   active: boolean
 }
 
-type ScheduleDetailProps = {
-  schedule: Schedule
+type ItemJob = {
+  id: string;
+  proposition_id: string;
+  type_proposition: string;
+  date_apresentation: string;
+  text: string;
+  author: string;
+  link: string;
+  status: string
 }
 
-export default function DetailSchedule({schedule}: ScheduleDetailProps) {
+type Job = {
+  id: string;
+  date_job: string;
+  schedule: Schedule;
+  items: ItemJob[]
+}
+
+type ScheduleDetailProps = {
+  schedule: Schedule
+  jobs: Job[]
+}
+
+export default function DetailSchedule({schedule, jobs}: ScheduleDetailProps) {
   const router = useRouter()
 
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
@@ -62,6 +81,12 @@ export default function DetailSchedule({schedule}: ScheduleDetailProps) {
     router.push('/schedules')
   }
 
+  async function handleRunJob() {
+    await api.post('/jobs/once',  {
+      schedule_id: schedule.id
+    })
+  }
+
   function openModalConfirm() {
     setIsOpenModal(true)
   }
@@ -91,6 +116,9 @@ export default function DetailSchedule({schedule}: ScheduleDetailProps) {
           </button>
           )
           }
+          <button onClick={handleRunJob} className="flex justify-center items-center uppercase  py-1 px-2 rounded-md text-sm bg-purple-400 text-white">
+            Executar
+          </button>
           <button onClick={openModalConfirm} className="flex justify-center items-center uppercase  py-1 px-2 rounded-md text-sm bg-red-400 text-white">
             Remover
           </button>
@@ -106,8 +134,42 @@ export default function DetailSchedule({schedule}: ScheduleDetailProps) {
         </li>
         <li><strong className="mr-2">Termos:</strong>{schedule.tags.map(tag => tag.name).join(', ')}</li>
       </ul>
-      
     </div>
+
+    {jobs.map(job => (
+      <div key={job.id} className="flex flex-1 flex-col rounded-md bg-gray-100 p-4 mt-4">
+        <div className="flex justify-between w-full items-center mb-8 ">
+          <h1 className="text-2xl font-normal text-gray-600">
+            Trabalho execuado em {job.date_job}
+          </h1>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Número</th>
+              <th>Tipo</th>
+              <th>Data de Apresentação</th>
+              <th>Ementa</th>
+              <th>Autor</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {job.items.map(item => (
+              <tr key={item.id}>
+                <td>{item.proposition_id}</td>
+                <td>{item.type_proposition}</td>
+                <td>{item.date_apresentation}</td>
+                <td>{item.text}</td>
+                <td>{item.author}</td>
+                <td>{item.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    ))}
+    
     <ConfirmModal
       title="Tem certeza desta ação?"
       isOpen={isOpenModal}
@@ -121,10 +183,11 @@ export default function DetailSchedule({schedule}: ScheduleDetailProps) {
 
 export const getServerSideProps: GetServerSideProps = withSSRAuth(async ({params}) => {
   const { id } = params;
-  const response = await api.get(`/schedules/${id}`)
-  const schedule = response.data
+  const {data: schedule} = await api.get(`/schedules/${id}`)
+
+  const { data: jobs } = await api.get(`/jobs/schedule/${id}`)
 
   return {
-    props: { schedule }
+    props: { schedule, jobs }
   }
 })
