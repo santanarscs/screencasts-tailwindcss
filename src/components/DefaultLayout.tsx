47364e737af9bf0d4e353bd0c5c0413c9ab6e59a
santanarscs <raphaelstn@gmail.com>
2021-07-19 11:11:48 -0300

    add animation

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from 'react'
+import { Transition } from '@headlessui/react'
+import { useRouter } from 'next/router'
+import { ReactNode, useEffect, useState } from 'react'
 import { Header } from './Header'
 import { Sidebar } from './Sidebar'
 
@@ -7,16 +9,27 @@ type LayoutProps = {
 }
 
 export const DefaultLayoutComponent = ({children}: LayoutProps) => {
-  
+  const [isShowing, setIsShowing] = useState(false)
+  const {asPath} = useRouter()
+  useEffect(() => {
+    setTimeout(() => {setIsShowing(true)}, 300)
+  },[asPath])
   return (
     <div className="flex flex-col min-h-screen">
-        <Header />
-        <div className="flex w-full my-6 mx-auto px-6 max-w-screen-2xl">
-          <Sidebar />
-          <div className="flex flex-1 flex-col">
+      <Header />
+      <div className="flex w-full my-6 mx-auto px-6 max-w-screen-2xl">
+        <Sidebar />
+        <div className="flex flex-1 flex-col">
+          <Transition
+            show={isShowing}
+            enter="transition-opacity duration-75"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
             {children}
-          </div>
+          </Transition>
         </div>
       </div>
+    </div>
   )
 }
