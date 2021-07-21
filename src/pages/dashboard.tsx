24d5ae3663458099280d5fcf69bc23484adf8fe0
santanarscs <raphaelstn@gmail.com>
2021-07-21 10:00:52 -0300

    add basic dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,23 @@
+import { GetServerSideProps } from "next";
 import { DefaultLayoutComponent } from "../components/DefaultLayout";
-
-export default function Dashboard() {
+import { withSSRAuth } from "../utils/withSSRAuth";
+import { getProviders, getSession } from "next-auth/client"
+export default function Dashboard({user}) {
   return (
     <DefaultLayoutComponent>
-      <h1>Dashboard</h1>
+      <div className="bg-white p-6 rounded-md shadow-sm">
+        <h1 className="text-2xl text-gray-600">Bem-vindo, {user.given_name}</h1>
+      </div>
     </DefaultLayoutComponent>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = withSSRAuth(async (ctx) => {
+  const { req } = ctx
+  const session = await getSession({req})
+  return {
+    props: {
+      user: session.user
+    }
+  }
+})
