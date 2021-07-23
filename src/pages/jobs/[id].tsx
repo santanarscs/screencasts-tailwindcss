82649a7baec5070bc7cfba01f465807674553af0
santanarscs <raphaelstn@gmail.com>
2021-07-23 13:55:59 -0300

    show message not data in detail jobs

--- src/pages/jobs/[id].tsx
+++ src/pages/jobs/[id].tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from "next";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { DefaultLayoutComponent } from "../../components/DefaultLayout";
 import { JobCongressSearchBox } from "../../components/JobCongressSearchBox";
 import { api } from "../../services/api";
@@ -79,6 +79,20 @@ export default function JobDetail({ job, status, types_propositions, authors }) 
 
     setJobs(finalJobs)
   }
+
+
+  if(!job.items.length) {
+    return (
+      <DefaultLayoutComponent>
+        <div className="flex flex-1 flex-col  text-gray-600">
+          <h1 className="text-2xl font-normal mb-3">Trabalho realizado em: {job.date_job}</h1>
+          <img className="h-28" src="/images/no_data.svg" alt="No data"/>
+          <h1 className="text-2xl text-center mt-6">Não foram encontradas propostas neste trabalho</h1>
+        </div>
+      </DefaultLayoutComponent>
+    )
+  }
+
   return (
     <DefaultLayoutComponent>
       <div className="flex flex-1 flex-col  text-gray-600">
@@ -87,42 +101,32 @@ export default function JobDetail({ job, status, types_propositions, authors }) 
         <div className=" flex flex-row-reverse my-3">
           <span className="bg-indigo-500 p-2 text-white text-xs rounded-md">Total de propostas: {jobs.length}</span>
         </div>
-        {!!jobs 
-          ? (
-            <table className="min-w-full divide-y divide-gray-200 ">
-            <thead className="bg-gray-50">
-              <tr>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Número</th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tipo</th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Data de Apresentação</th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Texto</th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">status</th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Autor</th>
+          <table className="min-w-full divide-y divide-gray-200 ">
+          <thead className="bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Número</th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tipo</th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Data de Apresentação</th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Texto</th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">status</th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Autor</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {jobs.map(item => (
+              <tr key={item.proposition_id}>
+                <td className="px-6 py-4 ">
+                  <a className="text-brand hover:text-brand-dark" rel="stylesheet" href={item.link} target="_blank">{item.proposition_id}</a>
+                </td>
+                <td className="px-6 py-4">{item.type_proposition}</td>
+                <td className="px-6 py-4">{item.date_apresentation}</td>
+                <td className="px-6 py-4">{item.text}</td>
+                <td className="px-6 py-4">{item.status}</td>
+                <td className="px-6 py-4">{item.author}</td>
               </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {jobs.map(item => (
-                <tr key={item.proposition_id}>
-                  <td className="px-6 py-4 ">
-                    <a className="text-brand hover:text-brand-dark" rel="stylesheet" href={item.link} target="_blank">{item.proposition_id}</a>
-                  </td>
-                  <td className="px-6 py-4">{item.type_proposition}</td>
-                  <td className="px-6 py-4">{item.date_apresentation}</td>
-                  <td className="px-6 py-4">{item.text}</td>
-                  <td className="px-6 py-4">{item.status}</td>
-                  <td className="px-6 py-4">{item.author}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          )
-          : (
-            <>
-              <img className="h-28" src="/images/no_data.svg" alt="No data"/>
-              <h1 className="text-2xl text-center mt-6">Não foram encontradas propostas neste trabalho</h1>
-            </>
-          )
-        }
+            ))}
+          </tbody>
+        </table>
       </div>
     </DefaultLayoutComponent>
   )
