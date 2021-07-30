665b74a6005325ff60404a8826b19d5563ca1ac3
santanarscs <raphaelstn@gmail.com>
2021-07-30 16:48:00 -0300

    update name

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,7 @@
 export function Logo() {
   return (
     <h1 className="text-3xl text-gray-800 md:text-2xl font-bold tracking-tight w-64">
-      monitor
+      uirapuru
       <span className="ml-1 text-blue-500">.</span>
     </h1>
   )
